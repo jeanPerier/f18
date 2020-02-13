e836f955210d3e7b11db6f7c35f2613355307008
Jean Perier <jperier@nvidia.com>
2020-02-13 06:52:24 -0800

    [review 959] Keep PFT dump human readable and rework test
    The parent field was not adding so much to the tests and it made dump
    harder to read for human where indentation is enough. Hence, it is removed.

--- test-lit/lower/pre-fir-tree01.f90
+++ test-lit/lower/pre-fir-tree01.f90
@@ -2,33 +2,35 @@
 
 ! Test structure of the Pre-FIR tree
 
-! CHECK: PFT root node:[[#%u, ROOT:]]
-! CHECK: [[#%u, FOO:]]{{.*}}Subroutine foo{{.*}}parent:[[#ROOT]]
+! CHECK: Subroutine foo
 subroutine foo()
-  ! CHECK: [[#%u, DO1:]]{{.*}}<<DoConstruct>>{{.*}}parent:[[#FOO]]
-  ! CHECK: NonLabelDoStmt{{.*}}parent:[[#DO1]]
+  ! CHECK: <<DoConstruct>>
+  ! CHECK: NonLabelDoStmt
   do i=1,5
-    ! CHECK: PrintStmt{{.*}}parent:[[#DO1]]
+    ! CHECK: PrintStmt
     print *, "hey"
-    ! CHECK: [[#%u, DO2:]]{{.*}}<<DoConstruct>>{{.*}}parent:[[#DO1]]
+    ! CHECK: <<DoConstruct>>
     do j=1,5
-      ! CHECK: PrintStmt{{.*}}parent:[[#DO2]]
+      ! CHECK: PrintStmt
       print *, "hello", i, j
-    ! CHECK: EndDoStmt{{.*}}parent:[[#DO2]]
+    ! CHECK: EndDoStmt
     end do
-  ! CHECK: EndDoStmt{{.*}}parent:[[#DO1]]
+    ! CHECK: <<EndDoConstruct>>
+  ! CHECK: EndDoStmt
   end do
-! CHECK: EndSubroutine
+  ! CHECK: <<EndDoConstruct>>
 end subroutine
+! CHECK: EndSubroutine foo
 
-! CHECK: BlockData{{.*}}parent:[[#ROOT]]
+! CHECK: BlockData
 block data
   integer, parameter :: n = 100
   integer, dimension(n) :: a, b, c
   common /arrays/ a, b, c
 end
+! CHECK: EndBlockData
 
-! CHECK: [[#%u, TEST_MOD:]]{{.*}}ModuleLike{{.*}}parent:[[#ROOT]]
+! CHECK: ModuleLike
 module test_mod
 interface
   ! check specification parts are not part of the PFT.
@@ -39,75 +41,89 @@ end interface
  integer :: xdim
  real, allocatable :: pressure(:)
 contains
-  ! CHECK: [[#%u, M_FOO:]]{{.*}}Subroutine foo{{.*}}parent:[[#TEST_MOD]]
+  ! CHECK: Subroutine foo
   subroutine foo()
     contains
-    ! CHECK: [[#%u, SUBFOO:]]{{.*}}Subroutine subfoo{{.*}}parent:[[#M_FOO]]
+    ! CHECK: Subroutine subfoo
     subroutine subfoo()
     end subroutine
-    ! CHECK: [[#%u, SUBFOO2:]]{{.*}}Function subfoo2{{.*}}parent:[[#M_FOO]]
+    ! CHECK: EndSubroutine subfoo
+    ! CHECK: Function subfoo2
     function subfoo2()
     end function
+    ! CHECK: EndFunction subfoo2
   end subroutine
+  ! CHECK: EndSubroutine foo
 
-  ! CHECK: [[#%u, M_FOO2:]]{{.*}}Function foo2{{.*}}parent:[[#TEST_MOD]]
+  ! CHECK: Function foo2
   function foo2(i, j)
     integer i, j, foo2
-    ! CHECK: AssignmentStmt{{.*}}parent:[[#M_FOO2]]
+    ! CHECK: AssignmentStmt
     foo2 = i + j
     contains
-    ! CHECK: [[#%u, SUBFOO:]]{{.*}}Subroutine subfoo{{.*}}parent:[[#M_FOO2]]
+    ! CHECK: Subroutine subfoo
     subroutine subfoo()
     end subroutine
+    ! CHECK: EndSubroutine subfoo
   end function
+  ! CHECK: EndFunction foo2
 end module
+! CHECK: EndModuleLike
 
-! CHECK: [[#%u, SUB_MOD:]]{{.*}}ModuleLike{{.*}}parent:[[#ROOT]]
+! CHECK: ModuleLike
 submodule (test_mod) test_mod_impl
 contains
-  ! CHECK: [[#%u, SUBM_FOO:]]{{.*}}Subroutine foo{{.*}}parent:[[#SUB_MOD]]
+  ! CHECK: Subroutine foo
   subroutine foo()
     contains
-    ! CHECK: [[#%u, SUBFOO:]]{{.*}}Subroutine subfoo{{.*}}parent:[[#SUBM_FOO]]
+    ! CHECK: Subroutine subfoo
     subroutine subfoo()
     end subroutine
-    ! CHECK: [[#%u, SUBFOO2:]]{{.*}}Function subfoo2{{.*}}parent:[[#SUBM_FOO]]
+    ! CHECK: EndSubroutine subfoo
+    ! CHECK: Function subfoo2
     function subfoo2()
     end function
+    ! CHECK: EndFunction subfoo2
   end subroutine
-  ! CHECK: [[#%u, MP_DUMP:]]{{.*}}MpSubprogram dump{{.*}}parent:[[#SUB_MOD]]
+  ! CHECK: EndSubroutine foo
+  ! CHECK: MpSubprogram dump
   module procedure dump
-    ! CHECK: FormatStmt{{.*}}parent:[[#MP_DUMP]]
+    ! CHECK: FormatStmt
 11  format (2E16.4, I6)
-    ! CHECK: [[#%u, IF1:]]{{.*}}<<IfConstruct>>{{.*}}parent:[[#MP_DUMP]]
-    ! CHECK: IfThenStmt{{.*}}parent:[[#IF1]]
+    ! CHECK: <<IfConstruct>>
+    ! CHECK: IfThenStmt
     if (xdim > 100) then
-      ! CHECK: PrintStmt{{.*}}parent:[[#IF1]]
+      ! CHECK: PrintStmt
       print *, "test: ", xdim
-    ! CHECK: ElseStmt{{.*}}parent:[[#IF1]]
+    ! CHECK: ElseStmt
     else
-      ! CHECK: WriteStmt{{.*}}parent:[[#IF1]]
+      ! CHECK: WriteStmt
       write (*, 11) "test: ", xdim, pressure
-    ! CHECK: EndIfStmt{{.*}}parent:[[#IF1]]
+    ! CHECK: EndIfStmt
     end if
+    ! CHECK: <<EndIfConstruct>>
   end procedure
 end submodule
+! CHECK: EndModuleLike
 
-! CHECK: BlockData{{.*}}parent:[[#ROOT]]
-block data
+! CHECK: BlockData
+block data named_block
  integer i, j, k
  common /indexes/ i, j, k
 end
+! CHECK: EndBlockData
 
-! CHECK: [[#%u, BAR:]]{{.*}}Function bar{{.*}}parent:[[#ROOT]]
+! CHECK: Function bar
 function bar()
 end function
+! CHECK: EndFunction bar
 
-! CHECK: [[#%u, PROG:]]{{.*}}Program <anonymous>{{.*}}parent:[[#ROOT]]
+! CHECK: Program <anonymous>
   ! check specification parts are not part of the PFT.
   ! CHECK-NOT: node
   use test_mod
   real, allocatable :: x(:)
-  ! CHECK: AllocateStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: AllocateStmt
   allocate(x(foo2(10, 30)))
 end
+! CHECK: EndProgram
--- test-lit/lower/pre-fir-tree04.f90
+++ test-lit/lower/pre-fir-tree04.f90
@@ -3,7 +3,7 @@
 ! Test Pre-FIR Tree captures all the coarray related statements
 
 ! CHECK: PFT root node:[[#%u, ROOT:]]
-! CHECK: [[#%u, PROG:]]{{.*}}Subroutine test_coarray{{.*}}parent:[[#ROOT]]
+! CHECK: Subroutine test_coarray
 Subroutine test_coarray
   use iso_fortran_env, only: team_type, event_type, lock_type
   type(team_type) :: t
@@ -12,55 +12,60 @@ Subroutine test_coarray
   real :: y[10,*]
   integer :: counter[*]
   logical :: is_master
-  ! CHECK: [[#%u, CHANGE_TEAM:]]{{.*}}ChangeTeamConstruct{{.*}}parent:[[#PROG]]
+  ! CHECK: <<ChangeTeamConstruct>>
   change team(t, x[5,*] => y)
-    ! CHECK: AssignmentStmt{{.*}}parent:[[#CHANGE_TEAM]]
+    ! CHECK: AssignmentStmt
     x = x[4, 1]
   end team
-  ! CHECK: FormTeamStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: <<EndChangeTeamConstruct>>
+  ! CHECK: FormTeamStmt
   form team(1, t)
 
-  ! CHECK: [[#%u, IF:]]{{.*}}IfConstruct{{.*}}parent:[[#PROG]]
+  ! CHECK: <<IfConstruct>>
   if (this_image() == 1) then
-    ! CHECK: EventPostStmt{{.*}}parent:[[#IF]]
+    ! CHECK: EventPostStmt
     event post (done)
   else
-    ! CHECK: EventWaitStmt{{.*}}parent:[[#IF]]
+    ! CHECK: EventWaitStmt
     event wait (done)
   end if
+  ! CHECK: <<EndIfConstruct>>
 
-  ! CHECK: [[#%u, CRITICAL:]]{{.*}}CriticalConstruct{{.*}}parent:[[#PROG]]
+  ! CHECK: <<CriticalConstruct>>
   critical
-    ! CHECK: AssignmentStmt{{.*}}parent:[[#CRITICAL]]
+    ! CHECK: AssignmentStmt
     counter[1] = counter[1] + 1
   end critical
+  ! CHECK: <<EndCriticalConstruct>>
 
-  ! CHECK: LockStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: LockStmt
   lock(alock)
-  ! CHECK: PrintStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: PrintStmt
   print *, "I have the lock"
-  ! CHECK: UnlockStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: UnlockStmt
   unlock(alock)
 
-  ! CHECK: SyncAllStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: SyncAllStmt
   sync all
-  ! CHECK: SyncMemoryStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: SyncMemoryStmt
   sync memory
-  ! CHECK: SyncTeamStmt{{.*}}parent:[[#PROG]]
+  ! CHECK: SyncTeamStmt
   sync team(t)
 
-  ! CHECK: [[#%u, IF2:]]{{.*}}IfConstruct{{.*}}parent:[[#PROG]]
+  ! CHECK: <<IfConstruct>>
   if (this_image() == 1) then
-    ! CHECK: SyncImagesStmt{{.*}}parent:[[#IF2]]
+    ! CHECK: SyncImagesStmt
     sync images(*)
   else
-    ! CHECK: SyncImagesStmt{{.*}}parent:[[#IF2]]
+    ! CHECK: SyncImagesStmt
     sync images(1)
   end if
+  ! CHECK: <<EndIfConstruct>>
 
-  ! CHECK: [[#%u, IF3:]]{{.*}}IfConstruct{{.*}}parent:[[#PROG]]
+  ! CHECK: <<IfConstruct>>
   if (y<0.) then
-    ! CHECK: FailImageStmt{{.*}}parent:[[#IF3]]
+    ! CHECK: FailImageStmt
    fail image
   end if
+  ! CHECK: <<EndIfConstruct>>
 end
